642e4123b9ed77625876962bbb94275854fc1306
Andrey Vladimirov <vappiter@yandex.ru>
2022-09-19 01:20:56 +0300

    CREATE TABLE

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import sqlalchemy
-import models
+from sqlalchemy.orm import sessionmaker
+from models.models import create_tables, Publisher, Book, Shop, Stook, Sale
+
+DSN = 'postgresql://postgres:123456@localhost:5432/PythonSQL'
+
+if __name__ == '__main__':
+    
+ engine = sqlalchemy.create_engine(DSN)
+ 
+ create_tables(engine)
+ 
+ Session = sessionmaker(bind=engine)
+ session = Session()
+ 
+ 
+ 
+ session.close()
+ 
--- models/models.py
+++ models/models.py
@@ -1,4 +1,3 @@
-from enum import unique
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
 
@@ -13,3 +12,35 @@ class Publisher (Base):
 class Book(Base):
     __tablename__ = "book"
     
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_publisher = sq. Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+    title = sq.Column (sq.String (length=40), unique = True)
+    
+    publisher = relationship(Publisher, backref = 'book')
+    
+class Shop(Base):
+    __tablename__ = 'shop'
+    
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column (sq.String (length=40), unique = True)
+    
+class Stook(Base):
+    __tablename__ = 'stook'
+    
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq. Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    id_shop = sq. Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+    count = sq.Column (sq.Integer)
+    
+class Sale(Base):
+    __tablename__ = 'sale'
+    
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_stook = sq. Column(sq.Integer, sq.ForeignKey('stook.id'), nullable=False)
+    price = sq.Column(sq.Float, nullable=False)
+    date_sale = sq.Column (sq.Date, nullable=False)
+    count = sq.Column (sq.Integer)
+    
+def create_tables(engine):
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
